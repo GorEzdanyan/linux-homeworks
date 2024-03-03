30ebff6d451e758e8e199baedfe00ba5780e970b
Gor <gor.ezdanyan@student.rau.am>
2024-03-03 16:06:23 +0400

    IPC-2

--- IPC-2/prime-calculator.cpp
+++ IPC-2/prime-calculator.cpp
@@ -5,6 +5,7 @@
 #include <sys/wait.h>
 #include <math.h>
 
+
 bool isprime(int n)
 {
 	for(int i = 2; i <= sqrt(n); ++i)
@@ -20,14 +21,21 @@ bool isprime(int n)
 
 int main()
 {
-	int pipe_fd[2];
-	int pipe_created = pipe(pipe_fd);
+	int pipe_fd1[2],pipe_fd2[2];
+	int pipe_created = pipe(pipe_fd1);
 	if(pipe_created == -1)
 	{
 		std::cerr << strerror(errno) << std::endl;
 		exit(errno);	
 	}
-
+	
+	pipe_created = pipe(pipe_fd2);
+	if(pipe_created == -1)
+	{
+		std::cerr << strerror(errno) << std::endl;
+		exit(errno);	
+	}
+	
 	pid_t pid = fork();
 	if(pid == -1)
 	{
@@ -39,38 +47,56 @@ int main()
 	
 	if(pid == 0)
 	{
-		
-		read(pipe_fd[0], &m, sizeof(int));
-		std::cout << "[Child] Calculating " << m << "-th prime number..." << std::endl;
-		
-		int tmp = m;
-		int i = 2;
-		while(tmp != 0)
+		close(pipe_fd1[1]);
+		close(pipe_fd2[0]);
+		while(true)
 		{
-			tmp -= isprime(i++);
+			read(pipe_fd1[0], &m, sizeof(int));
+			std::cout << "[Child] Calculating " << m << "-th prime number..." << std::endl;
+			
+			int tmp = m;
+			int i = 2;
+			while(tmp != 0)
+			{
+				tmp -= isprime(i++);
+			}
+			--i;
+			
+			std::cout << "[Child] Sending calculation result of prime(" << m << ")..." << std::endl; 
+			write(pipe_fd2[1], &i, sizeof(int));
 		}
-		--i;
 		
-		std::cout << "[Child] Sending calculation result of prime(" << m << ")..." << std::endl; 
-		write(pipe_fd[1], &i, sizeof(int));
-		close(pipe_fd[0]);
-		close(pipe_fd[1]);
+		close(pipe_fd1[0]);
+		close(pipe_fd2[1]);
 	}
 	else
 	{
+		close(pipe_fd1[0]);
+		close(pipe_fd2[1]);
+	
 		int result;
-		std::cout << "[Parent] Please enter the number: ";
-		std::cin >> m;
-		std::cout << "[Parent] Sending " << m << " to the child process..." << std::endl;
-		write(pipe_fd[1], &m, sizeof(int));
+		std::string _input;
 		
-		std::cout<< "[Parent] Waiting for the response from the child process..." << std::endl;
-		wait(NULL);
-		read(pipe_fd[0], &result, sizeof(int));
-		std::cout << "[Parent] Received calculation result of prime(" << m << ") = " << result << std::endl; 
+		while(true)
+		{
+			std::cout << "[Parent] Please enter the number: ";
+			std::cin >> _input;
+			if(_input == "exit")
+			{
+				kill(pid, SIGKILL);
+				exit(0);
+			}
+			
+			m = std::stoi(_input);
+			std::cout << "[Parent] Sending " << m << " to the child process..." << std::endl;
+			write(pipe_fd1[1], &m, sizeof(int));
+			std::cout<< "[Parent] Waiting for the response from the child process..." << std::endl;
+			read(pipe_fd2[0], &result, sizeof(int));
+			std::cout << "[Parent] Received calculation result of prime(" << m << ") = " << result << std::endl; 
+		}
 		
-		close(pipe_fd[0]);
-		close(pipe_fd[1]);
+		close(pipe_fd1[1]);
+		close(pipe_fd2[0]);
 	}
 	
 	return 0;
